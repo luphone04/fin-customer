51b1b68fe5f1bac9e841c23b3d99e522daff82b9
luphone04 <luphonemaw09@gmail.com>
2025-10-02 11:22:57 +0700

    root file change

--- app/page.js
+++ app/page.js
@@ -1,18 +1,13 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-export default function BoxBasic() {
-  return (
-    <main>
-      <Box component="section" className="border border-gray-800 m-5 text-center">
-        <h1 className="text-3xl text-violet-950">Stock Management v1.0</h1>
-        <ul>
-          <li><a href="/product">Products</a></li>
-          <li><a href="/category">Category</a></li>
-        </ul>
-        
-      </Box>
-    </main>
-  );
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/customer");
+  }, [router]);
+
+  return <div>Redirecting to Customer page...</div>;
 }
